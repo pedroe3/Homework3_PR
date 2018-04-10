5fcc4d72054a986a1630d7d6ace5ec0a21fa49e2
Pedro Rivera <pedrorivera@pedros-macbook-pro.local>
2018-04-09 20:38:14 -0400

    Initial Commit

---
+++ Homework3_PR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework3_PR
+//
+//  Created by Pedro Rivera on 4/9/18.
+//  Copyright © 2018 Pedro Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
